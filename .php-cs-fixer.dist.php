1db91314e5c17acaa72dd1dc26e39e9096dcb874
bzd <beslanov@gmail.com>
2026-03-28 23:19:03 +0300

    fix(ci): disable visibility_required in php-cs-fixer to prevent forcing public const under PHP 8.3 env

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -22,7 +22,7 @@ return (new PhpCsFixer\Config())
         'phpdoc_separation' => true,
         'phpdoc_trim' => true,
         'no_extra_blank_lines' => true,
-        'visibility_required' => ['elements' => ['property', 'method']],
+        'visibility_required' => false,
     ])
     ->setFinder($finder)
     ->setUsingCache(true);
